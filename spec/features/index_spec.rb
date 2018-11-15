5d665b9452e35b39138bc607ca002804a865647b
AndreasMN <andycutter@godmail.dk>
2018-11-15 16:02:35 +0100

    add basic test for content on landing page

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -7,5 +7,11 @@ describe 'Landing Page', type: :feature do
         expect(page).to have_content 'Viaplay'
     end
 
-    
+    it 'displays a header on landing page' do
+        expect(page).to have_css '#header'
+    end
+
+    it 'displays footer on landing page' do
+        expect(page).to have_css '#footer'
+    end
 end
