6931969831af2389565fc46e5d72a8b957fba32f
AndreasMN <andycutter@godmail.dk>
2018-11-19 15:27:14 +0100

    add styling for category div

--- source/javascripts/api.js
+++ source/javascripts/api.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch(proxyURL + apiURL).then(response => {
         response.json().then(viaplay => {
             console.log(viaplay)
-            let displayDiv = document.querySelector('#category')
+            let displayDiv = document.querySelector('.category')
             viaplay._links['viaplay:categoryFilters'].forEach(category => {
                 let categoryDiv = document.createElement('div')
                 categoryDiv.innerHTML = category.title
